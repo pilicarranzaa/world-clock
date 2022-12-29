273c07af271eeb9f34adfb1c44be02f2b13cc04d
pilicarranzaa <115506676+pilicarranzaa@users.noreply.github.com>
2022-12-29 11:55:05 +0100

    Added js for multiple locations

--- javascript/index.js
+++ javascript/index.js
@@ -1,24 +1,53 @@
+//update Time
+
 function updateTime(){
-//Los Angeles
-let losAngelesElement = document.querySelector("#los-angeles");
-let losAngelesDateElement = losAngelesElement.querySelector(".date");
-let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+    //Los Angeles
+    let losAngelesElement = document.querySelector("#los-angeles");
+    if (losAngelesElement) {
+        let losAngelesDateElement = losAngelesElement.querySelector(".date");
+        let losAngelesTimeElement = losAngelesElement.querySelector(".time");
 
-let losAngelesTime = moment().tz("America/Los_Angeles");
+        let losAngelesTime = moment().tz("America/Los_Angeles");
 
-losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
-losAngelesTimeElement.innerHTML = losAngelesTime.format("h:mm:ss [<small>]A[</small>]");
+        losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+        losAngelesTimeElement.innerHTML = losAngelesTime.format("h:mm:ss [<small>]A[</small>]");
+    }
 
-//Cordoba
-let cordobaElement = document.querySelector("#cordoba");
-let cordobaDateElement = cordobaElement.querySelector(".date");
-let cordobaTimeElement = cordobaElement.querySelector(".time");
+    //Cordoba
+    let cordobaElement = document.querySelector("#cordoba");
+    if (cordobaElement) {
+        let cordobaDateElement = cordobaElement.querySelector(".date");
+        let cordobaTimeElement = cordobaElement.querySelector(".time");
 
-let cordobaTime = moment().tz("America/Cordoba");
+        let cordobaTime = moment().tz("America/Cordoba");
 
-cordobaDateElement.innerHTML = cordobaTime.format("MMMM Do YYYY");
-cordobaTimeElement.innerHTML = cordobaTime.format("h:mm:ss [<small>]A[</small>]");
+        cordobaDateElement.innerHTML = cordobaTime.format("MMMM Do YYYY");
+        cordobaTimeElement.innerHTML = cordobaTime.format("h:mm:ss [<small>]A[</small>]");
+    }
 }
+
 
 updateTime();
 setInterval(updateTime, 1000);
+
+//update city
+
+function updateCity(event) {
+    let cityTimeZone = event.target.value;
+    let cityTime = moment().tz(cityTimeZone);
+    let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+    let citiesElement = document.querySelector("#cities");
+    citiesElement.innerHTML = `
+    <div class="city">
+        <div>
+            <h2>${cityName}</h2>
+            <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+        </div>
+        <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small></div>
+    </div>
+    `;
+}
+
+let citiesSelectElement = document.querySelector("#city");
+
+citiesSelectElement.addEventListener("change", updateCity);
